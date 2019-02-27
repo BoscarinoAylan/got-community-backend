a36651e0de039367b2c83d212ad1b388c8233a44
AylanBoscarino <aylandesousa@gmail.com>
2019-02-27 12:20:40 -0300

    :hammer: production sequelize dialect

--- src/core/config.js
+++ src/core/config.js
@@ -20,6 +20,7 @@ module.exports = {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             name: process.env.DB_NAME,
+            dialect: 'mysql'
         }
     }
 };
